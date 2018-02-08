914d3f2eedcbdfe829cb5e327de8b76bbff75ba1
Alan Aw <alanaw@DN0a25b048.SUNet>
2018-02-08 15:25:11 -0800

    Added the R files

---
+++ BoundsForResults.R
@@ -0,0 +1,293 @@
+####### R code for Theta Bounds and Difference Statistics #######
+####### Version 2.0: (8/30/2017)                          #######
+####### By Alan Aw                                        #######
+
+##### Bounds are mathematical, so 
+##### work for any mutation model 
+##### (infinite sites, infinite alleles) #####
+
+#### Paramaters ####
+S = 100 # Segregating Sites
+n = 50 # Individuals
+j = 1 # Component of SFS, must be in [1,n-1]
+k = 5 # Value of \xi_j, must be in [0,S]
+####
+
+###########################################
+######### Theta Estimator Bounds ##########
+###########################################
+
+#### Tajima's Pi ####
+f.max <- function(n,j) { # Helper function: get upper bound
+  f.max.out = 0 
+  for (i in 1:(n-1)) {
+    if (i!=j & i*(n-i) > f.max.out) {
+      f.max.out = i * (n-i)
+    }
+  }
+  return(f.max.out)
+}
+
+f.min <- function(n,j) { # Helper function: get lower bound
+  f.min.out = n * n 
+  for (i in 1:(n-1)) {
+    if (i!=j & i*(n-i) < f.min.out) {
+      f.min.out = i * (n-i)
+    }
+  }
+  return(f.min.out)
+}
+
+### Upper Bound ###
+f.thetaPi.max <- function(n,S,j,k) { 
+  (k*j*(n-j) + (S-k)*f.max(n,j)) / choose(n,2)
+}
+
+### Lower Bound ###
+f.thetaPi.min <- function(n,S,j,k) {
+  (k*j*(n-j) + (S-k)*f.min(n,j)) / choose(n,2)
+} 
+
+### Example Plots ###
+## Initialize some parameters ##
+n=10
+S=20
+j=1
+k=5
+
+## Let's plot dependence of
+## Tajima's Pi on k ##
+vec.k = c(0:S) 
+vec.thetaPi.max = f.thetaPi.max(n,S,j,vec.k)
+plot(vec.k/S,vec.thetaPi.max, xlab="Frequency of Derived Singletons", ylab=expression("Tajima's"~pi),col="blue",ylim=c(0,50),main=paste("n =",n,", S =", S),pch="o")
+lines(vec.k/S, vec.thetaPi.max, xlim=range(vec.k), ylim=range(vec.thetaPi.max), col="blue4",lwd=2,pch=16)
+vec.thetaPi.min = f.thetaPi.min(n,S,j,vec.k) 
+points(vec.k/S,vec.thetaPi.min,col="red",pch="o") 
+lines(vec.k/S, vec.thetaPi.min, xlim=range(vec.k), ylim=range(vec.thetaPi.min), col="darkred",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Tajima's Pi on n ##
+n.lower=3
+n.upper=50
+vec.n = c(n.lower:n.upper)
+vec.thetaPi.max=c(1:length(vec.n))
+for (i in 1:length(vec.n)) {
+  vec.thetaPi.max[i]=f.thetaPi.max(i+n.lower-1,S,j,k) 
+}
+plot(vec.n,vec.thetaPi.max, xlab="No. of Sampled Individuals", ylab="Tajima's Pi",col="purple",ylim=c(0,70),main=paste("k =",k,", S =",S),pch="o")
+lines(vec.n, vec.thetaPi.max, xlim=range(vec.n), ylim=range(vec.thetaPi.max), col="darkslateblue",lwd=2,pch=16)
+vec.ThetaPi.min=c(1:length(vec.n))
+for (i in 1:length(vec.n)) {
+  vec.thetaPi.min[i]=f.thetaPi.min(i+n.lower-1,S,j,k) 
+}
+points(vec.n,vec.thetaPi.min,col="green",pch="o") 
+lines(vec.n, vec.thetaPi.min, xlim=range(vec.n), ylim=range(vec.thetaPi.min), col="darkgreen",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Tajima's Pi on S ##
+vec.S = seq(from=k,to=100,by=1) 
+vec.thetaPi.max = f.thetaPi.max(n,vec.S,j,k)
+plot(vec.S,vec.thetaPi.max, xlab="No. of Segregating Sites", ylab="Tajima's Pi",col="orange",ylim=c(0,50),main=paste("k =",k,", n =",n),pch="o")
+lines(vec.S, vec.thetaPi.max, xlim=range(vec.S), ylim=range(vec.thetaPi.max), col="orange4",lwd=2,pch=16)
+vec.thetaPi.min = f.thetaPi.min(n,vec.S,j,k)
+points(vec.S,vec.thetaPi.min,col="pink",pch="o") 
+lines(vec.S, vec.thetaPi.min, xlim=range(vec.S), ylim=range(vec.thetaPi.min), col="pink4",lwd=2,pch=16)
+
+####
+
+#### Fay and Wu's H ####
+#### DO NOT RUN THIS PART IN CONJUNCTION 
+#### with Tajima's Pi code above! ####
+
+f.max <- function(n,j) { # Helper function: get upper bound
+  f.max.out = 0 
+  for (i in 1:(n-1)) {
+    if (i!=j & i*i > f.max.out) {
+      f.max.out = i*i
+    }
+  }
+  return(f.max.out)
+}
+
+f.min <- function(n,j) { # Helper function: get lower bound
+  f.min.out = n * n 
+  for (i in 1:(n-1)) {
+    if (i!=j & i*i < f.min.out) {
+      f.min.out = i*i
+    }
+  }
+  return(f.min.out)
+}
+
+### Upper Bound ###
+f.thetaH.max <- function(n,S,j,k) { 
+  (k*j*j + (S-k)*f.max(n,j)) / choose(n,2)
+}
+
+### Lower Bound ###
+f.thetaH.min <- function(n,S,j,k) {
+  (k*j*j + (S-k)*f.min(n,j)) / choose(n,2)
+}
+
+### Example Plots ###
+##
+n=10
+S=20
+j=1
+
+## Let's plot dependence of
+## Fay and Wu's H on k ##
+vec.k = c(0:S)
+vec.thetaH.max = f.thetaH.max(n,S,j,vec.k)
+plot(vec.k,vec.thetaH.max, xlab="k", ylab="Fay and Wu's H",col="blue",ylim=c(0,100),main=paste("n =",n,", S =",S,", j =",j))
+vec.thetaH.min = f.thetaH.min(n,S,j,vec.k)
+points(vec.k,vec.thetaH.min,col="red") 
+
+## Let's plot dependence of
+## Fay and Wu's H on S ##
+vec.S = seq(from=k,to=100,by=1) 
+vec.thetaH.max = f.thetaH.max(n,vec.S,j,k)
+plot(vec.S,vec.thetaH.max, xlab="S", ylab="Fay and Wu's H",col="purple",ylim=c(0,200),main=paste("n =",n,", j =",j,", k =",k))
+lines(vec.S, vec.thetaH.max, xlim=range(vec.S), ylim=range(vec.thetaH.max), col="darkslateblue",lwd=2,pch=16)
+vec.thetaH.min = f.thetaH.min(n,vec.S,j,k)
+points(vec.S,vec.thetaH.min,col="green") 
+lines(vec.S, vec.thetaH.min, xlim=range(vec.S), ylim=range(vec.thetaH.min), col="darkgreen",lwd=2,pch=16)
+
+
+###########################################
+########## Difference Statistics ##########
+###########################################
+
+#### Helper Functions ####
+little.a <- function(n) {
+  harm.sum = 0
+  for (i in 1:(n-1)) {
+    harm.sum = harm.sum + 1/i
+  }
+  return(harm.sum)
+}
+
+big.a <- function(n) {
+  harm.sum.square = 0
+  for (i in 1:(n-1)) {
+    harm.sum.square = harm.sum.square + 1/(i^2)
+  }
+  return(harm.sum.square)
+}
+
+### For Tajima's D ### (see Simonsen, Churchill, Aquadro, 1995)
+v.T <- function(n) {
+  (2*(n^2+n+3)/(9*n*(n-1))-(n+2)/(little.a(n)*n) + big.a(n)/(little.a(n))^2) / (little.a(n)^2+big.a(n))
+}
+
+u.T <- function(n) {
+  (((n+1)/(3*(n-1))-1/little.a(n))/little.a(n)) - v.T(n) 
+} ###
+
+### For Fu and Li's F ### (see Simonsen, Churchill, Aquadro, 1995)
+v.F <- function(n) {
+  ((2*n^3 + 110*n^2 -255*n + 153)/(9*n^2*(n-1))+(2*(n-1)*little.a(n))/(n^2)-(8*big.a(n))/n) / (little.a(n)^2+big.a(n))
+}
+
+u.F <- function(n) {
+  ((4*n^2+19*n+3-12*(n+1)*little.a(n+1))/(3*n*(n-1)))/little.a(n) - v.F(n)
+}
+#### Tajima's D ####
+
+### Upper Bound ###
+f.TajimaD.max <- function(n,S,j,k) {
+  return((f.thetaPi.max(n,S,j,k) - S/little.a(n)) / sqrt(u.T(n)*S + v.T(n)*S*S))
+}
+
+### Lower Bound ###
+f.TajimaD.min <- function(n,S,j,k) {
+  (f.thetaPi.min(n,S,j,k) - S/little.a(n)) / sqrt(u.T(n)*S + v.T(n)*S*S)
+}
+
+## Let's plot dependence of
+## Tajima's D on frequency of singletons, k ##
+vec.k = c(0:S)
+vec.TajimaD.max = f.TajimaD.max(n,S,j,vec.k)
+plot(vec.k/S,vec.TajimaD.max, xlab="Frequency of Derived Singletons", ylab="Tajima's D",col="blue",ylim=c(-4,8),main=paste("n =",n,", S =",S),pch="o")
+lines(vec.k/S, vec.TajimaD.max, xlim=range(vec.k), ylim=range(vec.TajimaD.max), col="blue4",lwd=2,pch=16)
+vec.TajimaD.min = f.TajimaD.min(n,S,j,vec.k)   
+points(vec.k/S,vec.TajimaD.min,col="red",pch="o") 
+lines(vec.k/S, vec.TajimaD.min, xlim=range(vec.k), ylim=range(vec.TajimaD.min), col="darkred",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Tajima's D on S ##
+vec.S = seq(from=k,to=200,by=1) 
+vec.TajimaD.max = f.TajimaD.max(n,vec.S,j,k)
+plot(vec.S,vec.TajimaD.max, xlab="No. of Segregating Sites", ylab="Tajima's D",col="orange",xlim=range(vec.S), ylim=range(-4,8),main=paste("k =",k,", n =",n),pch="o")
+lines(vec.S, vec.TajimaD.max, xlim=range(vec.S), ylim=range(vec.TajimaD.max), col="orange4",lwd=2,pch=16)
+vec.TajimaD.min = f.TajimaD.min(n,vec.S,j,k)
+points(vec.S,vec.TajimaD.min,col="pink",pch="o") 
+lines(vec.S, vec.TajimaD.min, xlim=range(vec.S), ylim=range(vec.TajimaD.min), col="pink4",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Tajima's D on n ##
+n.lower=3
+n.upper=50
+vec.n = c(n.lower:n.upper)
+vec.TajimaD.max=c(1:length(vec.n))
+for (i in 1:length(vec.n)) {
+  vec.TajimaD.max[i]=f.TajimaD.max(i+n.lower-1,S,j,k) 
+}
+plot(vec.n,vec.TajimaD.max, xlab="No. of Sampled Individuals", ylab="Tajima's D",col="purple",xlim=range(vec.n), ylim=range(-4,6),main=paste("k =",k,", S =",S),pch="o")
+lines(vec.n, vec.TajimaD.max, xlim=range(vec.n), ylim=range(vec.TajimaD.max), col="darkslateblue",lwd=2,pch=16)
+vec.TajimaD.min=c(1:length(vec.n))
+for (i in 1:length(vec.n)) {
+  vec.TajimaD.min[i]=f.TajimaD.min(i+n.lower-1,S,j,k) 
+}
+points(vec.n,vec.TajimaD.min,col="green",pch="o") 
+lines(vec.n, vec.TajimaD.min, xlim=range(vec.n), ylim=range(vec.TajimaD.min), col="darkgreen",lwd=2,pch=16)
+
+#### Fu and Li's F ####
+
+### Upper Bound ###
+f.FuLiF.max <- function(n,S,j,k) {
+  (f.thetaPi.max(n,S,j,k) - k) / sqrt(u.F(n)*S + v.F(n)*S*S)
+}
+
+### Lower Bound ###
+f.FuLiF.min <- function(n,S,j,k) {
+  (f.thetaPi.min(n,S,j,k) - k) / sqrt(u.F(n)*S + v.F(n)*S*S)
+}
+
+## Let's plot dependence of
+## Fu and Li's F on frequency of singletons \xi_1 ##
+vec.k = c(0:S)
+vec.FuLiF.max = f.FuLiF.max(n,S,j,vec.k)
+plot(vec.k/S,vec.FuLiF.max, xlab="Frequency of Derived Singletons", ylab="Fu and Li's F",col="blue",ylim=c(-12,6),main=paste("n =",n,", S =",S),pch="o")
+lines(vec.k/S, vec.FuLiF.max, xlim=range(vec.k/S), ylim=range(vec.FuLiF.max), col="blue4",lwd=2,pch=16)
+vec.FuLiF.min = f.FuLiF.min(n,S,j,vec.k)   
+points(vec.k/S,vec.FuLiF.min,col="red",pch="o") 
+lines(vec.k/S, vec.FuLiF.min, xlim=range(vec.k/S), ylim=range(vec.FuLiF.min), col="darkred",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Fu and Li's F on S ##
+vec.S = seq(from=k,to=200,by=1) 
+vec.FuLiF.max = f.FuLiF.max(n,vec.S,j,k)
+plot(vec.S,vec.FuLiF.max, xlab="No. of Segregating Sites", ylab="Fu and Li's F",col="orange",xlim=range(vec.S), ylim=range(-12,6),main=paste("k =",k,", n =",n),pch="o")
+lines(vec.S, vec.FuLiF.max, xlim=range(vec.S), ylim=range(vec.FuLiF.max), col="orange4",lwd=2,pch=16)
+vec.FuLiF.min = f.FuLiF.min(n,vec.S,j,k)
+points(vec.S,vec.FuLiF.min,col="pink",pch="o") 
+lines(vec.S, vec.FuLiF.min, xlim=range(vec.S), ylim=range(vec.FuLiF.min), col="pink4",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Fu and Li's F on n ##
+n.lower=3
+n.upper=100
+vec.n = c(n.lower:n.upper)
+vec.FuLiF.max=c(1:length(vec.n))
+for (i in 1:length(vec.n)) {
+  vec.FuLiF.max[i]=f.FuLiF.max(i+n.lower-1,S,j,k) 
+}
+plot(vec.n,vec.FuLiF.max, xlab="No. of Sampled Individuals", ylab="Fu and Li's F",col="purple",xlim=range(vec.n), ylim=range(-4,12),main=paste("k =",k,", S =",S),pch="o")
+lines(vec.n, vec.FuLiF.max, xlim=range(vec.n), ylim=range(vec.FuLiF.max), col="darkslateblue",lwd=2,pch=16)
+vec.FuLiF.min=c(1:length(vec.n))
+for (i in 1:length(vec.n)) {
+  vec.FuLiF.min[i]=f.FuLiF.min(i+n.lower-1,S,j,k) 
+}
+points(vec.n,vec.FuLiF.min,col="green",pch="o") 
+lines(vec.n, vec.FuLiF.min, xlim=range(vec.n), ylim=range(vec.FuLiF.min), col="darkgreen",lwd=2,pch=16)
---
+++ PlotDataWithBounds.R
@@ -0,0 +1,82 @@
+####### R code for Plotting Data with Bounds #######
+####### Version 1.0: (8/30/2017)     #######
+####### By Alan Aw                   #######
+
+#### Helper Functions ####
+little.a <- function(n) {
+  harm.sum = 0
+  for (i in 1:(n-1)) {
+    harm.sum = harm.sum + 1/i
+  }
+  return(harm.sum)
+}
+
+big.a <- function(n) {
+  harm.sum.square = 0
+  for (i in 1:(n-1)) {
+    harm.sum.square = harm.sum.square + 1/(i^2)
+  }
+  return(harm.sum.square)
+}
+
+### For Tajima's D ### (see Simonsen, Churchill, Aquadro, 1995)
+v.T <- function(n) {
+  (2*(n^2+n+3)/(9*n*(n-1))-(n+2)/(little.a(n)*n) + big.a(n)/(little.a(n))^2) / (little.a(n)^2+big.a(n))
+}
+
+u.T <- function(n) {
+  (((n+1)/(3*(n-1))-1/little.a(n))/little.a(n)) - v.T(n) 
+} ###
+
+#### Tajima's D ####
+
+### Upper Bound ###
+f.TajimaD.max <- function(n,S,j,k) {
+  return((f.thetaPi.max(n,S,j,k) - S/little.a(n)) / sqrt(u.T(n)*S + v.T(n)*S*S))
+}
+
+### Lower Bound ###
+f.TajimaD.min <- function(n,S,j,k) {
+  (f.thetaPi.min(n,S,j,k) - S/little.a(n)) / sqrt(u.T(n)*S + v.T(n)*S*S)
+}
+
+## Let's plot dependence of
+## Tajima's Pi on k ##
+vec.k = c(0:S) 
+vec.thetaPi.max = f.thetaPi.max(n,S,j,vec.k)
+plot(vec.k/S,vec.thetaPi.max, xlab="Frequency of Derived Singletons", ylab="",col="blue",ylim=c(0,50),main=paste("n =",n,", S =", S),type="n")
+lines(vec.k/S, vec.thetaPi.max, xlim=range(vec.k), ylim=range(vec.thetaPi.max), col="blue4",lwd=2,pch=16)
+vec.thetaPi.min = f.thetaPi.min(n,S,j,vec.k) 
+points(vec.k/S,vec.thetaPi.min,col="red",type="n") 
+lines(vec.k/S, vec.thetaPi.min, xlim=range(vec.k), ylim=range(vec.thetaPi.min), col="darkred",lwd=2,pch=16)
+
+# Copy and paste: expression("Tajima's"~pi), Frequency of Derived Singletons
+
+## Let's plot dependence of
+## Tajima's D on frequency of singletons, k ##
+vec.k = c(0:S)
+vec.TajimaD.max = f.TajimaD.max(n,S,j,vec.k)
+plot(vec.k/S,vec.TajimaD.max, xlab="", ylab="",col="blue",ylim=c(-4,8),main=paste("n =",n,", S =",S),type="n")
+lines(vec.k/S, vec.TajimaD.max, xlim=range(vec.k), ylim=range(vec.TajimaD.max), col="blue4",lwd=2,pch=16)
+vec.TajimaD.min = f.TajimaD.min(n,S,j,vec.k)   
+points(vec.k/S,vec.TajimaD.min,col="red",type="n") 
+lines(vec.k/S, vec.TajimaD.min, xlim=range(vec.k), ylim=range(vec.TajimaD.min), col="darkred",lwd=2,pch=16)
+
+## Let's plot dependence of
+## Fu and Li's F on frequency of singletons, k ##
+vec.k = c(0:S)
+vec.FuLiF.max = f.FuLiF.max(n,S,j,vec.k)
+plot(vec.k/S,vec.FuLiF.max, xlab="Frequency of Derived Singletons", ylab="",col="blue",ylim=c(-12,6),main=paste("n =",n,", S =",S),type="n")
+lines(vec.k/S, vec.FuLiF.max, xlim=range(vec.k/S), ylim=range(vec.FuLiF.max), col="blue4",lwd=2,pch=16)
+vec.FuLiF.min = f.FuLiF.min(n,S,j,vec.k)   
+points(vec.k/S,vec.FuLiF.min,col="red",type="n") 
+lines(vec.k/S, vec.FuLiF.min, xlim=range(vec.k/S), ylim=range(vec.FuLiF.min), col="darkred",lwd=2,pch=16)
+
+# copy and paste: Fu and Li's F, "Frequency of Derived Singletons"
+
+# Paste this in: "Frequency of Derived Singletons" "Tajima's D"
+## Let's add the simulated data ##
+points(k.vec,diffStat.vec,col="black",pch="o")
+
+df = read.table("fuliF-bottleneckN500S100.txt",header=FALSE)
+points(as.vector(df$V1[-1]),as.vector(df$V2[-1]),col="black",pch="o")
