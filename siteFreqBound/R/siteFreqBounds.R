9da23dc22fecaaa0afeeca3deb1a46f3b95683b3
Alan Aw <alanaw@DN0a226205.SUNet>
2018-03-17 21:12:36 -0700

    Adding finished package

---
+++ siteFreqBound/R/siteFreqBounds.R
@@ -0,0 +1,36 @@
+#' Package: siteFreqBounds
+#'
+#' A collection of functions plotting model-independent bounds on summary statistics of the site frequency
+#' spectrum. See examples
+#' for functions \code{\link{f.min}} and  \code{\link{f.max}}.
+#' @seealso \code{\link{f.min}}
+#' @seealso \code{\link{f.max}}
+#' @seealso \code{\link{f.thetaPi.max}}
+#' @seealso \code{\link{f.thetaPi.min}}
+#' @seealso \code{\link{plot_tajima_Pi_k}}
+#' @seealso \code{\link{plot_tajima_Pi_n}}
+#' @seealso \code{\link{plot_tajima_Pi_S}}
+#' @seealso \code{\link{little.a}}
+#' @seealso \code{\link{big.a}}
+#' @seealso \code{\link{v.T}}
+#' @seealso \code{\link{u.T}}
+#' @seealso \code{\link{v.F}}
+#' @seealso \code{\link{u.F}}
+#' @seealso \code{\link{f.TajimaD.max}}
+#' @seealso \code{\link{f.TajimaD.min}}
+#' @seealso \code{\link{plot_tajima_D_k}}
+#' @seealso \code{\link{plot_tajima_D_S}}
+#' @seealso \code{\link{plot_tajima_D_n}}
+#' @seealso \code{\link{f.FuLi.max}}
+#' @seealso \code{\link{f.FuLi.min}}
+#' @seealso \code{\link{plot_Fu_Li_k}}
+#' @seealso \code{\link{plot_statistic}}
+#' @seealso \code{\link{plot_n}}
+#' @seealso \code{\link{plot_S}}
+#' @seealso \code{\link{plot_Fu_li_S}}
+#' @seealso \code{\link{plot_Fu_li_n}}
+#' @seealso \code{\link{getCommand}}
+#' @seealso \code{\link{runCommand}}
+#' @docType package
+#' @name siteFreqBounds
+NULL
